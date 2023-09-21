1fb0fd727519b561d5e54e80b10411dab3ddd6ed
renya-cpu <renya@example.com>
2023-09-21 00:32:34 +0000

    create get_param func for ssm

---
+++ ssm/get_parameters.py
@@ -0,0 +1,15 @@
+import boto3
+
+
+def get_parameters(param_key):
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameters(
+        Names=[
+            param_key,
+        ],
+        WithDecryption=True
+    )
+    return response['Parameters'][0]['Value']
+
+param_name = "test-kujira"
+print(get_parameters(param_name))
